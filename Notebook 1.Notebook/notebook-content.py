75f4383c0d86c028829e4f41cce5ec1e806e27c5
yaridtx <yari@dtx.nl>
2025-02-07 09:49:52 +0100

    Committing 3 items from workspace 9c2b8565-8f1f-4836-8519-673e948474da

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,27 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "a69f4775-8afd-4dbd-93dd-3bf2e376b897",
+# META       "default_lakehouse_name": "test",
+# META       "default_lakehouse_workspace_id": "9c2b8565-8f1f-4836-8519-673e948474da"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+select * From test
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
